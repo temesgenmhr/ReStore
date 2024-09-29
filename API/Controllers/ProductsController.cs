a5537724703a4b7fe40e682cb251eaf8b13ae432
temesgen <tmsmhr@gmail.com>
2024-09-29 11:02:06 +0300

    end of sec 4

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers
 {
     [ApiController]
-    [Route("apo/[controller]")]
+    [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
         private readonly StoreContext _context;
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -6,7 +6,7 @@ export default function Catalog() {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/apo/Products")
+    fetch("http://localhost:5000/api/Products")
       .then((response) => response.json())
       .then((data) => setProducts(data));
   }, []);
